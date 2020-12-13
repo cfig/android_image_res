5ff0d54252367d6bae8c8448dc0100c98823a212
cfig <yuyezhong@gmail.com>
2020-12-13 16:55:13 +0800

    enable strict mode cpio checking

--- 10.0.0_coral-qq1d.200205.002/test.py
+++ 10.0.0_coral-qq1d.200205.002/test.py
@@ -57,9 +57,9 @@ def vbmeta_only():
     decompressXZ("src/integrationTest/resources/10.0.0_coral-qq1d.200205.002/boot.img.xz", "boot.img")
     subprocess.check_call("./gradlew unpack", shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("aaf8d027cb0c165de7c66bf32f8341c1", hashFile("boot.img.google"))
-    unittest.TestCase().assertEqual("0fed36e951788e49c2bcd23b95011878", hashFile("boot.img.signed"))
-    unittest.TestCase().assertEqual("0fed36e951788e49c2bcd23b95011878", hashFile("boot.img.signed2"))
+    unittest.TestCase().assertEqual("a619ad39e982c83b76a407f08e82a5e1", hashFile("boot.img.google"))
+    unittest.TestCase().assertEqual("078cef4c9debb2a1abfcc596e8291477", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("078cef4c9debb2a1abfcc596e8291477", hashFile("boot.img.signed2"))
 
 def boot_vbmeta():
     cleanUp()
@@ -67,8 +67,8 @@ def boot_vbmeta():
     decompressXZ("src/integrationTest/resources/10.0.0_coral-qq1d.200205.002/boot.img.xz", "boot.img")
     subprocess.check_call("./gradlew unpack", shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("0fed36e951788e49c2bcd23b95011878", hashFile("boot.img.signed"))
-    unittest.TestCase().assertEqual("70689c8e0e576ea6cc0b4301931304ab", hashFile("vbmeta.img.signed"))
+    unittest.TestCase().assertEqual("078cef4c9debb2a1abfcc596e8291477", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("5d07532769fec2003c1e4475a094e30c", hashFile("vbmeta.img.signed"))
     cleanUp()
 
 def boot_change_algorithm():
@@ -81,43 +81,43 @@ def boot_change_algorithm():
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 1/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("2adfd5d17b12a49b14e49a14504ab5eb", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("43e2e04dc59bcde1c172b0ab5c815016", hashFile("boot.img.signed"))
     # algorithm_type = 2
     subprocess.check_call(
 """
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 2/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("4ecb4d4493dea6480f93502fbd5491e3", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("e188f2865e683988e51a7d057738d04a", hashFile("boot.img.signed"))
     # algorithm_type = 3
     subprocess.check_call(
 """
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 3/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("7971607e61c7f55fa1c59c7ccbf042ac", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("c2b2a9b2e587b81ced78e836b0dfaced", hashFile("boot.img.signed"))
     # algorithm_type = 4
     subprocess.check_call(
 """
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 4/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("8e2e28b4bd69b1887ddc664ad75b99ed", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("a392c4368e6a5e94d1b7b32483ba3914", hashFile("boot.img.signed"))
     # algorithm_type = 5
     subprocess.check_call(
 """
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 5/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("b8bdacb1131e013404996ba7ace89ad3", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("832f12556acb4979fcf8ea556064d95e", hashFile("boot.img.signed"))
     # algorithm_type = 6
     subprocess.check_call(
 """
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/algorithm_type"\s*:\s\+\d/algorithm_type" : 6/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("c7488be18b6acaf1bd4f3191a0bbcb95", hashFile("boot.img.signed"))
-    unittest.TestCase().assertEqual("c7488be18b6acaf1bd4f3191a0bbcb95", hashFile("boot.img.signed2"))
+    unittest.TestCase().assertEqual("e17f440c19b52a072328f7da09a2b5c4", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("e17f440c19b52a072328f7da09a2b5c4", hashFile("boot.img.signed2"))
 
 def boot_change_footer_hash_algorithm():
     cleanUp()
@@ -128,8 +128,8 @@ def boot_change_footer_hash_algorithm():
 vim -u NONE -N build/unzip_boot/boot.avb.json -c '%s/hash_algorithm.*"/hash_algorithm" : "sha512"/g' -c ':wq'
 """.strip(), shell = True)
     subprocess.check_call("./gradlew pack", shell = True)
-    unittest.TestCase().assertEqual("5e23cf961963916b106799e2534c49fb", hashFile("boot.img.signed"))
-    unittest.TestCase().assertEqual("5e23cf961963916b106799e2534c49fb", hashFile("boot.img.signed2"))
+    unittest.TestCase().assertEqual("13fe1ffe8f13dc94eda387d1d65099e9", hashFile("boot.img.signed"))
+    unittest.TestCase().assertEqual("13fe1ffe8f13dc94eda387d1d65099e9", hashFile("boot.img.signed2"))
 
 if __name__ == "__main__":
     boot_only()
